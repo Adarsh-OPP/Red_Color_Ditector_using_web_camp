214937667abe4fd6dc721aebd1ae2a7d06335661
Adarsh Kumar <adarshkmrina@gmail.com>
2025-04-20 14:39:58 +0530

    Add files via upload

---
+++ Red_Color_Ditector.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+
+import numpy as np
+
+web = cv.VideoCapture(0)
+while True:
+    isTrue, img = web.read()
+    if isTrue:
+        hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
+        Red_L = np.array([135,85,115],np.uint8)
+        Red_U = np.array([185,250,250],np.uint8)
+        Red_M = cv.inRange(hsv,Red_L,Red_U)
+        kernal = np.ones((5,5),np.uint8)
+        Red_M = cv.dilate(Red_M,kernal)
+        red = cv.bitwise_and(img,img,mask = Red_M)
+        cont, hier = cv.findContours(Red_M,cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
+        for pic, cont in enumerate(cont):
+            area = cv.contourArea(cont)
+            if (area>500):
+                x,y,w,h = cv.boundingRect(cont)
+                img = cv.rectangle(img,(x,y),(x+w,y+h),(0,0,225),2)
+                cv.putText(img,"RED Color",(x,y),cv.FONT_HERSHEY_SIMPLEX,1.0,(0,0,255))
+        cv.imshow("Red_coloe_Detector",img)
+
+
+        if cv.waitKey(20) & 0xff == ord('q'):
+            break
+    else:
+        break
+web.release()
+cv.destroyAllWindows()
